4098fb7e645731d442f38175753574f9512eb532
Stanislav Basarab <stanislav.basarab@infotech.gov.ua>
2024-12-03 15:20:17 +0200

    feat(data): added repos

---
+++ src/main/java/org/spring/datingsite/data/InvitationRepository.java
@@ -0,0 +1,10 @@
+package org.spring.datingsite.data;
+
+import org.spring.datingsite.domain.Invitation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface InvitationRepository extends JpaRepository<Invitation, UUID> {}
